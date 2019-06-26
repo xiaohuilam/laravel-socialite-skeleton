fd22fa8fe444fca6c1e1f998929345f5f3aa246f
xiaohui.lam <xiaohui.lam@icloud.com>
2019-06-26 18:37:38 +0800

    init

---
+++ src/SocialiteSkeletonServiceProvider.php
@@ -0,0 +1,11 @@
+<?php
+namespace Xiaohuilam\Laravel\SocialiteSkeleton;
+
+use Illuminate\Support\ServiceProvider;
+
+class SocialiteSkeletonServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+    }
+}
